093a699f4bd2b3177020ba78f20ba1f0575099c0
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2022-09-18 22:54:59 +0100

    whileloop4 task solved

--- Loops/whileloop4/Solution.java
+++ Loops/whileloop4/Solution.java
@@ -26,17 +26,38 @@ package whileloop4;
 
 public class Solution {
     public static void main(String[] args) {
-            int middleStart = 0;
-            while (middleStart < 1) {
-                int middleSpace = 0;
-                while (middleSpace < 10) {
+        int firstStringB = 0;
+        while (firstStringB < 20) {
+            System.out.print("B");
+            firstStringB++;
+        }
+        System.out.println();
+        int length = 0;
+        while (length < 8) {
+            int lastLetterB = 0;
+            while (lastLetterB < 1) {
+                int firstLetterB = 0;
+                while (firstLetterB < 1) {
                     System.out.print("B");
-                    middleSpace++;
+                    firstLetterB++;
+                    int spaces = 0;
+                    while (spaces < 18) {
+                        System.out.print(" ");
+                        spaces++;
+                    }
                 }
-                System.out.println();
-                middleStart++;
+                System.out.print("B");
+                lastLetterB++;
             }
-
+            System.out.println();
+            length++;
+        }
+        int lastStringB = 0;
+        while (lastStringB < 20) {
+            System.out.print("B");
+            lastStringB++;
         }
     }
+}
+
 
