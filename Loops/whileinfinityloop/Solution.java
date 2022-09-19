0223879c5d3e3c378b7f2ced0a64dcce9ec69b4f
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2022-09-19 09:33:36 +0100

    Task Infinite loop solved

--- Loops/whileinfinityloop/Solution.java
+++ Loops/whileinfinityloop/Solution.java
@@ -18,7 +18,14 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner console = new Scanner(System.in);
 
+        while (true) {
+            String s = console.nextLine();
+            if (s.equals("exit")) {
+                break;
+            }
+            System.out.println(s);
+        }
     }
 }
