5237577ce8a6fcb9bb9066dc02cc1f692d53bc78
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2022-09-25 22:07:50 +0100

    Examples for loop added

---
+++ Loops/forloopcontinue/Solution.java
@@ -0,0 +1,43 @@
+package forloopcontinue;
+import java.util.Scanner;
+
+/*
+В методе main с клавиатуры считывается 3 целых числа: start, end (start <= end), multiple.
+Допиши программу, чтобы на экран выводилась сумма чисел от start (включительно) до end
+(не включительно), кратных multiple.
+Для этого используй цикл for.
+Подсказка: чтобы перейти к следующей итерации цикла, используй оператор continue.
+
+Требования:
+•	Программа должна вывести в консоль сумму чисел от start (включительно) до end (не включительно),
+кратных multiple.
+•	В программе необходимо использовать цикл for.
+•	В цикле for необходимо использовать оператор continue.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+        int multiple = scanner.nextInt();
+
+        int sum = 0;
+        //напишите тут ваш код
+        for (int i = start; i < end; i++) {
+            if (i % multiple != 0) {
+                continue;
+            }
+            sum = sum + i;}
+
+        System.out.println(sum);
+    }
+}
+/*
+int sum = 0;
+        for (int i = start; i < end; i++) {
+            if (i % multiple != 0) {
+                continue;
+            }
+            sum = sum + i;
+ */
