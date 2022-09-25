5e0621e524d67746d73acbf22f8704f3326ea7b6
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2022-09-25 22:57:08 +0100

    For loop triangle better solution

--- Loops/forlooptriangle/Solution.java
+++ Loops/forlooptriangle/Solution.java
@@ -4,12 +4,10 @@ package forlooptriangle;
 public class Solution {
     public static void main(String[] args) {
 
-        int length = 0;
-        for (int j = 0; j <= 10; j++) {
-            for (int i = 0; i < length; i++)
+        for (int j = 1; j <= 10; j++) {
+            for (int i = 0; i < j; i++)
                 System.out.print(i);
             System.out.println();
-            length++;
         }
     }
 }
